ea6d17289b365ace6c2020dc09b174a5ad8d8a10
Ahmed Roberts <52471253+ahmedroberts@users.noreply.github.com>
2024-08-12 10:11:49 -0700

    :tada: Modern Portfolio :rocket:

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,8 +7,8 @@ import { ThemeProvider } from "./provider";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Adrian's Portfolio",
-  description: "Modern & Minimal JS Mastery Portfolio",
+  title: "Ahmed Roberts's Portfolio",
+  description: "Full stack developer portfolio",
 };
 
 export default function RootLayout({
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -57,7 +57,7 @@ const Hero = () => {
           />
 
           <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">
-            Hi! I&apos;m Adrian, a Next.js Developer based in Croatia.
+            Hello! I&apos;m Ahmed, Developer based in Phoenix, AZ, USA.
           </p>
 
           <a href="#about">
